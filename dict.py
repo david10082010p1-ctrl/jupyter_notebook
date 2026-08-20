cd81483c4679a51eb9dd59f0c2757250b859098c
david10082010p1-ctrl <david1008.2010p1@gmail.com>
2026-08-19 21:56:30 -0300

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,20 @@
+pedidos = [
+    #objeto 1
+    {
+    "id": [1, 2, 3],
+    "cliente": ["João", "Maria", "Pedro"],
+    "tp_produto": ["A", "B", "C"],
+    "valor": [100.0, 200.0, 300.0],
+    "qtde" : [5,8,20],
+    "relatorio" : {       
+    "resultado" : ["Aprovado", "Reprovado", "Aprovado"],
+    "problemas encontrados" : ["Nenhum", "Falta de estoque", "Nenhum"],
+    "status_entrega" : ["Entregue", "Pendente", "Entregue"]
+    }
+    
+    }
+    #onjeto 2
+    
+    
+    
+    ]
